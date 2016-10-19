d7d36bcdc7ac36ec7c60c6407e15e4eac891ebd9
YouAreMyEyes <a113006161@163.com>
2016-10-19 15:20:34 +0800

    初始化项目框架

---
+++ dangdang-app-angularjs/assets/api/service.js
@@ -0,0 +1,17 @@
+//创建一个公用的数据调用服务
+app.factory('commonService',['#http',function($http){
+  var service={};
+  service.getData=function(type,callBack){
+    $http({
+      url:'../../data/book_'+type+'.json',
+      method:'get'
+    })
+    .then(function(res){
+      console.log('数据获取成功');
+      callBack(res);
+    },function(err){
+      console.dir(err);
+    })
+  }
+  return service
+}]);
