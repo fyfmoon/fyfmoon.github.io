f21b8cac76acb2ca86c53f6b34c2e2034f100430
YouAreMyEyes <a113006161@163.com>
2016-10-19 15:23:54 +0800

    111

--- dangdang-app-angularjs/assets/api/service.js
+++ dangdang-app-angularjs/assets/api/service.js
@@ -3,7 +3,7 @@ app.factory('commonService',['#http',function($http){
   var service={};
   service.getData=function(type,callBack){
     $http({
-      url:'../../data/book_'+type+'.json',
+      url:'/dangdang-app-angularjs/data/book_'+type+'.json',
       method:'get'
     })
     .then(function(res){
